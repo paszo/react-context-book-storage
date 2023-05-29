6a06a91e6bc150546626327aae26d2c090045fa3
paszo <paszo2008@gmail.com>
2023-05-29 08:20:28 +0200

    added reducer and local storage:

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,23 +1,20 @@
-import {createContext, useState} from 'react';
-
+import {createContext, useReducer, useEffect} from 'react';
+import {bookReducer} from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
-        {title: 'name of the wind', author: 'patrick rothfus', id: 1},
-        {title: 'the final empire', author: 'brandon sanderson', id:2}
-    ]);
-    const addBook = (title, author) => {
-        setBooks([...books, {title, author, id: Math.ceil(Math.random() * 100000)}])
-    }
+    const [books, dispatch] = useReducer(bookReducer,[], () => {
+        const localData = localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : [];
+    });
 
-    const removeBook = (id) => {
-        setBooks(books.filter(book => book.id !== id));
-    }
+    useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books));
+    }, [books])
 
     return (
-        <BookContext.Provider value={{books, addBook, removeBook}}>
+        <BookContext.Provider value={{books, dispatch}}>
             {props.children}
         </BookContext.Provider>
     )
